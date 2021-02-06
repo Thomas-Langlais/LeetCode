d11149b9cb827a01aaeddd8eacb8cb8cc27bfc64
Thomas L'Anglais <thomas1446@gmail.com>
2021-02-06 15:18:23 -0500

    Got the optimal sol, by removing the poppedItem queue and used a queue state size to determine when to exit the loop when finding the level items

--- Search/BTLevelOrderTrav.java
+++ Search/BTLevelOrderTrav.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 public class BTLevelOrderTrav {
     
     public static void main(String[] args) {
-        System.out.println(new Solution().levelOrder(
+        System.out.println(new SolutionV2().levelOrder(
             new TreeNode(
                 3, 
                 null,
@@ -54,7 +54,7 @@ public class BTLevelOrderTrav {
  *     }
  * }
  */
-class Solution {
+class SolutionV1 {
     
     public List<List<Integer>> levelOrder(TreeNode root) {
         
@@ -65,12 +65,17 @@ class Solution {
             return result;
         }
 
+        // setup the queue to pop nodes out with the root to start
         Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
         nodeQueue.add(root);
+        
+        // loop over the queue until we have traversed the tree
         while (!nodeQueue.isEmpty()) {
+
             // set the items
             List<Integer> levelitems = new LinkedList<Integer>();
             List<TreeNode> poppedNodes = new LinkedList<>();
+            
             // loop the current level elements
             for (double i = 0; i < Math.pow(2, level); i++) {
                 if (!nodeQueue.isEmpty()) {
@@ -92,6 +97,56 @@ class Solution {
             result.add(levelitems);
             // increment to the level to adjust the loop for the dequer
             level++;
+        }
+        return result;
+    }
+}
+
+class SolutionV2 {
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> result = new LinkedList<>();
+        // we now don't need the level, since we use the queue size state
+        // to determine our exit point
+        // double level = 0;
+
+        // exit if there is no root node to traverse
+        if (root == null) {
+            return result;
+        }
+
+        // setup the queue to pop nodes out with the root to start
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.add(root);
+        
+        // loop over the queue until we have traversed the tree
+        while (!nodeQueue.isEmpty()) {
+
+            // set the items
+            List<Integer> levelitems = new LinkedList<Integer>();
+            // check the current size of the queue, we will use this as our
+            // exit condition. we want to know the state of how many nodes are
+            // in the queue, so we can immediately insert the children in the
+            // queue so we can reduce the memory used by removing V1's poppedNOdes list
+            int currentQueueState = nodeQueue.size();
+            
+            // loop the current level elements
+            for (; currentQueueState > 0; currentQueueState--) {
+                TreeNode node = nodeQueue.poll();
+                // add the node value to the level
+                levelitems.add(node.val);
+                // append the node's child elements into the queue
+                // if they exist
+                if (node.left != null) {
+                    nodeQueue.add(node.left);
+                }
+                if (node.right != null) {
+                    nodeQueue.add(node.right);
+                }
+            }
+            
+            result.add(levelitems);
         }
         return result;
     }
